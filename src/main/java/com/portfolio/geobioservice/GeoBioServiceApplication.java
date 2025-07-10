ee710ea2a1a0de312349b84e8b4aac8a263d425e
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 11:08:49 -0300

    Create GeoBioServiceApplication.java

---
+++ src/main/java/com/portfolio/geobioservice/GeoBioServiceApplication.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.geobioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoBioServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeoBioServiceApplication.class, args);
+    }
+}
