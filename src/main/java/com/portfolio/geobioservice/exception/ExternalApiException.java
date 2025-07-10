d3b14702fdd880360026d06cb9374863296d952b
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 11:34:27 -0300

    Create ExternalApiException.java

---
+++ src/main/java/com/portfolio/geobioservice/exception/ExternalApiException.java
@@ -0,0 +1,8 @@
+package com.portfolio.geobioservice.exception;
+
+public class ExternalApiException extends RuntimeException {
+    public ExternalApiException(String message) {
+        super(message);
+    }
+}
+
